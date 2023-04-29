732cb4d60c1851a322dc2924581e14235ee9d2b0
Akhmedov96 <ahmedovnahid@icloud.com>
2023-04-29 12:32:17 +0500

    Initial Commit

---
+++ Do Homework Traffic Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Do Homework Traffic Light
+//
+//  Created by Нахид  Ахмедов on 29/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
